6b28952705ea7779260fc097c0e342d3e183e16b
sylvestre Marie <sylvestre.marie@gmail.com>
2018-06-25 17:08:31 +0200

    integration twig 2

--- src/Marie/LouvresBundle/Controller/DefaultController.php
+++ src/Marie/LouvresBundle/Controller/DefaultController.php
@@ -8,10 +8,24 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/",)
+     * @Route("/", name="accueil")
      */
     public function indexAction()
     {
         return $this->render('@MarieLouvres/Default/information.html.twig');
+    }
+    /**
+     * @Route("/", name="reservation")
+     */
+    public function reservationAction()
+    {
+        return $this->render('@MarieLouvres/Default/reservation.html.twig');
+    }
+    /**
+     * @Route("/", name="paiement")
+     */
+    public function paiementAction()
+    {
+        return $this->render('@MarieLouvres/Default/paiement.html.twig');
     }
 }
