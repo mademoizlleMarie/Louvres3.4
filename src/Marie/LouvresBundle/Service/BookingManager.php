c3f86b1f3793cdb90e325de79cbf168154d6fd5c
littleBlackMan <littleblackman@users.noreply.github.com>
2018-11-28 18:35:32 +0100

    factorise
    factorize the class

--- src/Marie/LouvresBundle/Service/BookingManager.php
+++ src/Marie/LouvresBundle/Service/BookingManager.php
@@ -12,13 +12,14 @@ class BookingManager
 {
     private $session;
     private $em;
-
+    
+    const REDUCE_PRICE = 10;
 
     //créez une __construct()méthode avec un $sessionargument qui possède l'indicateur de type (chemin d'accès). Définissez cette $sessionpropriété sur une nouvelle propriété et utilisez-la plus tard
     public function __construct( SessionInterface $session, EntityManagerInterface $em) // injecter l'entity manager
     {
        $this->session = $session;
-       $this->em = $em;
+       $this->em      = $em;
     }
 
     // mettre le chemin de l'entité permet de typer la variable = c'est un objet de type reservation
@@ -33,35 +34,26 @@ class BookingManager
     {
         // Je recupere la valeur de la methode getReservationId dans l'objet booking manager et vérifie que la valeur existe
         if ( $id = $this->getReservationId())
-
         {
             return $this->em->getRepository(Reservation::class)->find($id);
-        }
-
-        else {  //sinon je fais un find dans la bdd via doctrine*/
-
+        } else {  //sinon je fais un find dans la bdd via doctrine*/
             return $this->session->get('reservation');
-
         }
     }
 
     public function updateBooking(reservation $reservation)
     {
-        // recupérer les billets
         $tickets = $reservation->getTickets();
-        //calcul prix
-        $total = $this->calculPriceTotal($tickets);
-        //par defaut payement à false
+        $total   = $this->calculPriceTotal($tickets);
         $payment = $reservation->getPayment()=== false;
-        // ---------------------------------------- le code du billet revoir comment faire pour faire un code complet
-        $code = $reservation->getNumberofticket().$reservation->getName().date("YmdHis");//streplace pour enlever les espaces
-        var_dump($code);
-        //J'affecte la valeur à la proprieté $reservation
+        $code    = $reservation->getNumberofticket().$reservation->getName().date("YmdHis");
+
         $reservation->setPrice($total)->setPayment($payment)->setCode($code);
         // On boucle sur les tickets pour les lier à la reservation
-        foreach ($tickets as $ticket){$reservation->addTicket($ticket);}
+        foreach ($tickets as $ticket){
+            $reservation->addTicket($ticket);
+        }
         return $reservation;
-
     }
 
 
@@ -73,8 +65,6 @@ class BookingManager
         }
         return $total;
     }
-
-    const REDUCE_PRICE = 10;
 
     public function calculPriceTicket($ticket)
     {
@@ -92,19 +82,13 @@ class BookingManager
 
     public function setReservationId($id)
     {
-        //Je recupere l'objet session de symfony et j'affecte la valeur reservationId à l'attribut
         $this->session->set('reservationId', $id);
-
-        // revient au même que :
-        //$session = $this->session;
-        //$session->set('reservationId',$id);
-
     }
 
     public function getReservationId()
     {
-        if ($id = $this->session->get('reservationId')) return $id;
-       return null;
+       if (!$id = $this->session->get('reservationId')) return null;
+       return $id;
     }
 
     public function payementValide()
